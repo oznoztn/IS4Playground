6e194cfa345c2397f72e2d0b7bfb6a003965da05
oznoztn <goldenhawkk@gmail.com>
2020-09-17 18:10:33 +0300

    Create PolicyController

--- src/RawCodingAuth.Basics/Controllers/HomeController.cs
+++ src/RawCodingAuth.Basics/Controllers/HomeController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Abstractions;
 
-namespace RawCodingAuth.Basics.Controllers.Home
+namespace RawCodingAuth.Basics.Controllers
 {
     public class HomeController : Controller
     {
@@ -103,79 +98,6 @@ namespace RawCodingAuth.Basics.Controllers.Home
         public IActionResult Secret()
         {
             return View();
-        }
-
-        // User.Claims.Any(t => t.Type == ClaimTypes.Role) == true
-        //   olan ClaimsPrinciap için...
-        //   Kısacası kullanıcının 'role' claimine bakıyor, belirtilen rol var mı yok mu diye.
-        [Authorize(Roles = "god-mode")]
-        public IActionResult GodMode()
-        {
-            return Content("God mode");
-        }
-
-        [Authorize(Policy = "secret-garden")]
-        public IActionResult SecretGardenPolicy()
-        {
-            return Content("Secret Garden Policy");
-        }
-
-        [Authorize(Policy = "admin")]
-        public IActionResult CustomPolicy()
-        {
-            return Content("Custom Policy");
-        }
-
-        // Episode 4 - IAuthorizationService kullanımı
-
-        public async Task<IActionResult> AuthorizationService(
-            [FromServices]IAuthorizationService authorizationService)
-        {
-            // Poliçe oluşturuyorum. Normalde bu iş Startup içerisinde yapılır.
-            var policyBuilder = new AuthorizationPolicyBuilder();
-            var policy = 
-                policyBuilder
-                    .RequireClaim(ClaimTypes.Role, "admin")
-                    .Build();
-
-            ClaimsPrincipal loggedInUser = HttpContext.User;
-
-            var authzResult = await authorizationService.AuthorizeAsync(loggedInUser, null, policy);
-            if (authzResult.Succeeded)
-            {
-                // kullanıcı admin rolünde, bir şeyler yap.
-                return Content("You are the administrator!");
-            }
-            else
-            {
-                // kullanıcı admin rolünde değil, bir şeyler yap.
-                return Content("You are not the administrator!");
-            }
-        }
-
-        [Authorize]
-        public async Task<IActionResult> AuthorizationService2(
-            [FromServices]IAuthorizationService authorizationService)
-        {
-            // the policy name to check against
-            string policyName = "admin";
-; 
-            var result =
-                await authorizationService
-                    .AuthorizeAsync(HttpContext.User, null, policyName);
-
-            if (result.Succeeded)
-            {
-                // kullanıcı admin rolünde, bir şeyler yap.
-
-                return Content("You are the administrator!");
-            }
-            else
-            {
-                // kullanıcı admin rolünde değil, bir şeyler yap.
-
-                return Content("You are not the administrator!");
-            }
         }
     }
 }
---
+++ src/RawCodingAuth.Basics/Controllers/PolicyController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RawCodingAuth.Basics.Controllers
+{
+    public class PolicyController : Controller
+    {
+        // User.Claims.Any(t => t.Type == ClaimTypes.Role) == true
+        //   olan ClaimsPrinciap için...
+        //   Kısacası kullanıcının 'role' claimine bakıyor, belirtilen rol var mı yok mu diye.
+        [Authorize(Roles = "god-mode")]
+        public IActionResult GodMode()
+        {
+            return Content("God mode");
+        }
+
+        [Authorize(Policy = "secret-garden")]
+        public IActionResult SecretGardenPolicy()
+        {
+            return Content("Secret Garden Policy");
+        }
+
+        [Authorize(Policy = "admin")]
+        public IActionResult CustomPolicy()
+        {
+            return Content("Custom Policy");
+        }
+
+        // Episode 4 - IAuthorizationService kullanımı
+
+        public async Task<IActionResult> AuthorizationService(
+            [FromServices] IAuthorizationService authorizationService)
+        {
+            // Poliçe oluşturuyorum. Normalde bu iş Startup içerisinde yapılır.
+            var policyBuilder = new AuthorizationPolicyBuilder();
+            var policy =
+                policyBuilder
+                    .RequireClaim(ClaimTypes.Role, "admin")
+                    .Build();
+
+            ClaimsPrincipal loggedInUser = HttpContext.User;
+
+            var authzResult = await authorizationService.AuthorizeAsync(loggedInUser, null, policy);
+            if (authzResult.Succeeded)
+            {
+                // kullanıcı admin rolünde, bir şeyler yap.
+                return Content("You are the administrator!");
+            }
+            else
+            {
+                // kullanıcı admin rolünde değil, bir şeyler yap.
+                return Content("You are not the administrator!");
+            }
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AuthorizationService2(
+            [FromServices] IAuthorizationService authorizationService)
+        {
+            // the policy name to check against
+            string policyName = "admin";
+            ;
+            var result =
+                await authorizationService
+                    .AuthorizeAsync(HttpContext.User, null, policyName);
+
+            if (result.Succeeded)
+            {
+                // kullanıcı admin rolünde, bir şeyler yap.
+
+                return Content("You are the administrator!");
+            }
+            else
+            {
+                // kullanıcı admin rolünde değil, bir şeyler yap.
+
+                return Content("You are not the administrator!");
+            }
+        }
+    }
+}
