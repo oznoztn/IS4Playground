d6ff7effc794af8d39c00f429ac87ee721ec84c3
oznoztn <goldenhawkk@gmail.com>
2020-09-17 21:06:58 +0300

    5.5 - Validate issued JWT token and authorize the bearer

--- src/OAuthBasics.Server/Startup.cs
+++ src/OAuthBasics.Server/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace OAuthBasics.Server
 {
@@ -14,11 +17,18 @@ namespace OAuthBasics.Server
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            IdentityModelEventSource.ShowPII = true;
+
             services
                 .AddAuthentication("OAuth")
                 .AddJwtBearer("OAuth", config =>
                 {
-
+                    config.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = Constants.Issuer,
+                        ValidAudience = Constants.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Constants.SecretKey))
+                    };
                 });
 
             services.AddAuthorization();
