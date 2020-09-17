c5ad0d04710abb44c34b5fc10dba7a61c092868a
oznoztn <goldenhawkk@gmail.com>
2020-09-17 20:49:18 +0300

    5.2 - Setup bare minimum authc/authz requirements

--- src/OAuthBasics.Server/Startup.cs
+++ src/OAuthBasics.Server/Startup.cs
@@ -12,10 +12,18 @@ namespace OAuthBasics.Server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddAuthentication("OAuth")
+                .AddJwtBearer("OAuth", config =>
+                {
+
+                });
+
+            services.AddAuthorization();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +35,12 @@ namespace OAuthBasics.Server
             }
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
