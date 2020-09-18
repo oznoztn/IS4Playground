589e2219a9412e62d09db24a4c6bd3a117ee93a9
oznoztn <goldenhawkk@gmail.com>
2020-09-18 20:07:13 +0300

    6.1 - Create an empty project

---
+++ src/OAuthBasics.Client/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuthBasics.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Authenticate()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Secret()
+        {
+            return View();
+        }
+    }
+}
