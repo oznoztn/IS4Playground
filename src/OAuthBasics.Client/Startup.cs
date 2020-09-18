68bc01498f0f8e64e3d0f4ed125e071c621292d0
oznoztn <goldenhawkk@gmail.com>
2020-09-18 22:43:13 +0300

    6.2 - Implementing OAuth Client/Server Part I

--- src/OAuthBasics.Client/Startup.cs
+++ src/OAuthBasics.Client/Startup.cs
@@ -12,13 +12,64 @@ namespace OAuthBasics.Client
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddAuthentication(config =>
+                {
+                    // 1) authentication için ne kullanacaðýz
+                    //      CEVAP: Cookie, more specifically, OAuthBasics.Client.Cookie
+                    config.DefaultAuthenticateScheme = "OAuthBasics.Client.Cookie";
+
+                    // 2) sign-in olunduðunda ne olacak
+                    //      CEVAP: This will also use a cookie, more specifically, the OAuthBasics.Client.Cookie.
+                    config.DefaultSignInScheme = "OAuthBasics.Client.Cookie";
+
+                    // 3) authorization nasýl yapýlacak
+                    //      use this one to check if we are allowed to do something
+                    // Önceki örneklerde bu da  gibi olsaydý, default challenge scheme OAuthBasics.Client.Cookie olurdu.
+                    //      (NOT: Bunu o senaryolarda explicit olarak set etmemiþtik)
+                    // Burada set ediyoruz çünkü challenge iþlemi için bizim OAuthBasics.Server'a gideceðiz. 
+                    //      OAuthBasics.Client.Cookie'yi almak için!
+                    // Bu aþamaya geldiðinde artýk .AddOAuth() kýsmýndaki mantýk devreye girecek.
+                    // Ve orada oauth server bilgilerini tanýmlayacaðýz
+                    //      Ýsimlerin, OAuthBasics.Server, eþleþtiðine dikkat et!
+                    config.DefaultChallengeScheme = "OAuthBasics.Server";
+                })
+                .AddCookie("OAuthBasics.Client.Cookie")
+                .AddOAuth("OAuthBasics.Server", oauthOptions =>
+                {
+                    // REQUIREMENTS: CallbackPath, CliendId & ClientSecret, AuthorizationEndpoint, TokenEndpoint
+
+                    oauthOptions.ClientId = "OAuthBasics_Client_Id";
+                    oauthOptions.ClientSecret = "OAuthBasics_Client_Secret";
+
+                    // 1)
+                    // Burasý sanýrým bize Authorization Code'u verecek endpoint.
+                    //      Eðer öyleyse tabii ki Auth sunucusunda bulunan bir yer olacak:
+                    oauthOptions.AuthorizationEndpoint = "https://localhost:44324/oauth/authorize";
+
+                    // 2)
+                    // Bu adres middleware'in içerisinde bir yerde.
+                    // Bir blackbox, içeride ne oluyor bilmemize gerek yok.
+                    // Kullanýcý server tarafýndan authenticated olduðunda kullanýcýnýn redired edildiði adres.
+                    // Tek bilmemiz gereken "code" ve "state" bilgisini iþleyen adres burasý
+                    //      OAuthBasics.Server > OAuthorizeController.Authorize [POST] redirects here.
+                    //      Burasý iþini bitirdikten sonra access token için auth sunucusundaki
+                    //          (altta TokenEndpoint'de belirttiðimiz)
+                    //              token issue yapan endpointe istekte bulunuyor.
+                    //                  Dokümantasyona bakabilirsin.
+                    oauthOptions.CallbackPath = "/oauth/callback"; 
+
+                    // 3)
+                    // Auth sunucusundan dönen Authorization Kodu'na karþýlýk hangi endpoint'ten TOKEN alacaðýz?
+                    //      Tabii ki token issuer olan OAuth sunucusunda bir yere karþýlýk gelecek.
+                    oauthOptions.TokenEndpoint = "https://localhost:44324/oauth/token";
+                });
+
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,13 +78,11 @@ namespace OAuthBasics.Client
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
---
+++ src/OAuthBasics.Server/Controllers/OAuthController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace OAuthBasics.Server.Controllers
+{
+    public class RedirectionInfo
+    {
+        public string RedirectUri { get; set; }
+        public string State { get; set; }
+    }
+
+    public class OAuthController : Controller
+    {
+        // https://tools.ietf.org/html/rfc6749#section-4.2.1 4.2.1.  Authorization Request
+        // User authc için buraya yönlendirildiğiinde OAuthBasics.Client tarafı gönderiyor bunları
+        [HttpGet]
+        public async Task<IActionResult> Authorize(
+            string response_type, // authorization flow type
+            string client_id, 
+            string redirect_uri, // url to redirect after successful authentication
+            string scope, // what info I want
+            string state // client generates it. https://pipedrive.readme.io/docs/marketplace-oauth-authorization-state-parameter
+            )
+        {
+            return View(new RedirectionInfo
+            {
+                RedirectUri = redirect_uri,
+                State = state
+            });
+        }
+
+        
+        [HttpPost]
+        public async Task<IActionResult> Authorize(
+            string username, 
+            string password, 
+            string state, 
+            string redirect_uri)
+        {
+            // we assume the user info is valid.
+            
+            const string code = "auth code"; // we assume we generated the code
+
+            // // https://tools.ietf.org/html/rfc6749#section-4.1.2 Authorization Response
+
+            QueryBuilder query = new QueryBuilder();
+            query.Add(nameof(code), code);
+            query.Add(nameof(state), state);
+
+            string redirectUri = $"{redirect_uri}{query}";
+
+            return Redirect(redirectUri);
+
+
+            return null;
+        }
+
+        public async Task<IActionResult> Token()
+        {
+            return null;
+        }
+    }
+}
