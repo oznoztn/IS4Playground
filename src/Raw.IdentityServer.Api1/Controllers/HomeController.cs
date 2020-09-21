b64ed7ae1155fc1a70220bcb97fb0754c082d4f6
oznoztn <goldenhawkk@gmail.com>
2020-09-21 20:11:55 +0300

    9.0 - Create empty projects

---
+++ src/Raw.IdentityServer.Api1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Raw.IdentityServer.Api1.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            var okResult = Ok("Raw.IdentityServer.Api1 says Hello!");
+            
+            return await Task.FromResult(okResult);
+        }
+    }
+}
---
+++ src/Raw.IdentityServer.Api2/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Raw.IdentityServer.Api2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new
+            {
+                message = "Secret Message from Api2"
+            });
+        }
+    }
+}
