2affbfe5ae817f26711e0a0a37dae08ffb6aa7d6
oznoztn <goldenhawkk@gmail.com>
2020-09-24 23:38:16 +0300

    Create RawClientName.cs

---
+++ src/Raw.IdentityServer.Constants/RawClientName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Raw.IdentityServer.Constants
+{
+    public class RawClientName
+    {
+        /// <summary>
+        /// Raw.IdentityServer.Api1
+        /// </summary>
+        public const string Api1 = "Raw.IdentityServer.Api1";
+
+        /// <summary>
+        /// Raw.IdentityServer.Api2
+        /// </summary>
+        public const string Api2 = "Raw.IdentityServer.Api2";
+    }
+}
