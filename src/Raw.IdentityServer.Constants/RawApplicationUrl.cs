ba61ef83b0de54f78994c5b256ad4c8e3f42766b
oznoztn <goldenhawkk@gmail.com>
2020-09-24 23:37:31 +0300

    Create RawApplicationUrl.cs

---
+++ src/Raw.IdentityServer.Constants/RawApplicationUrl.cs
@@ -0,0 +1,25 @@
+﻿namespace Raw.IdentityServer.Constants
+{
+    public static class RawApplicationUrl
+    {
+        /// <summary>
+        /// https://localhost:44348/
+        /// </summary>
+        public const string IdentityServer = "https://localhost:44348/";
+
+        /// <summary>
+        /// https://localhost:44347/
+        /// </summary>
+        public const string Api1 = "https://localhost:44347/";
+
+        /// <summary>
+        /// https://localhost:44372/
+        /// </summary>
+        public const string Api2 = "https://localhost:44372/";
+
+        /// <summary>
+        /// https://localhost:44399/
+        /// </summary>
+        public const string Mvc = "https://localhost:44399/";
+    }
+}
