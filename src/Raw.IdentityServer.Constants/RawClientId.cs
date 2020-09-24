a5506f6fc7be94f503de5fa85666d3f5780daa77
oznoztn <goldenhawkk@gmail.com>
2020-09-24 23:38:02 +0300

    Create RawClientId.cs

---
+++ src/Raw.IdentityServer.Constants/RawClientId.cs
@@ -0,0 +1,15 @@
+﻿namespace Raw.IdentityServer.Constants
+{
+    public static class RawClientId
+    {
+        /// <summary>
+        /// Raw.IdentityServer.Mvc.ClientId
+        /// </summary>
+        public const string Mvc = "Raw.IdentityServer.Mvc.ClientId";
+
+        /// <summary>
+        /// Raw.IdentityServer.Api1.ClientId
+        /// </summary>
+        public const string Api1 = "Raw.IdentityServer.Api1.ClientId";
+    }
+}
