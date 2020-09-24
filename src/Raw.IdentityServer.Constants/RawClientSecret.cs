4b5a588faecf8993fc39290be84fe0918f4e5298
oznoztn <goldenhawkk@gmail.com>
2020-09-24 23:39:43 +0300

    Create RawClientSecret.cs

---
+++ src/Raw.IdentityServer.Constants/RawClientSecret.cs
@@ -0,0 +1,15 @@
+﻿namespace Raw.IdentityServer.Constants
+{
+    public class RawClientSecret
+    {
+        /// <summary>
+        /// Raw.IdentityServer.Mvc.ClientSecret
+        /// </summary>
+        public const string Mvc = "Raw.IdentityServer.Mvc.ClientSecret";
+
+        /// <summary>
+        /// Raw.IdentityServer.Api1.ClientSecret
+        /// </summary>
+        public const string Api1 = "Raw.IdentityServer.Api1.ClientSecret";
+    }
+}
